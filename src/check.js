5e8be3f9fa43e283aab273b62774dbd7febcd987
Tsutomu Kawamura <cognitom@gmail.com>
2017-04-09 10:49:13 +0900

    Fix style

--- src/check.js
+++ src/check.js
@@ -3,7 +3,7 @@ import hid from 'node-hid'
 export default function () {
   console.log('Checking your HID devices...')
   const stringify = JSON.stringify
-  const devices = hid.devices()
+  hid.devices()
     .map(device => ({
       manufacturer: device.manufacturer,
       product: device.product,
@@ -15,7 +15,7 @@ export default function () {
       const b2 = stringify(b)
       return (a2 === b2) ? 0 : (a2 < b2) ? -1 : 1
     })
-    .filter((cur, pos, arr) => !pos || stringify(cur) != stringify(arr[pos - 1]))
+    .filter((cur, pos, arr) => !pos || stringify(cur) !== stringify(arr[pos - 1]))
     .forEach(device => {
       console.log('')
       console.log(`# ${device.product} by ${device.manufacturer || 'n/a'}`)
